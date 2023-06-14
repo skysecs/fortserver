7fcace87463d9379f2b82261e1fc320ec287b5a1
skysecs <100838405+skysecs@users.noreply.github.com>
2023-06-14 08:50:06 +0000

    perf: 更新发布机的单用户单会话策略

--- apps/terminal/models/applet/applet.py
+++ apps/terminal/models/applet/applet.py
@@ -138,7 +138,7 @@ class Applet(JMSBaseModel):
         name = manifest['name']
         instance = cls.objects.filter(name=name).first()
         serializer = AppletSerializer(instance=instance, data=manifest)
-        serializer.is_valid()
+        serializer.is_valid(raise_exception=True)
         instance = serializer.save(builtin=builtin)
         instance.load_platform_if_need(path)
 
@@ -194,7 +194,8 @@ class Applet(JMSBaseModel):
         host = self.select_host(user)
         if not host:
             return None
-        can_concurrent = self.can_concurrent or self.type == 'web'
+        host_concurrent = str(host.deploy_options.get('RDS_fSingleSessionPerUser', 0)) == '0'
+        can_concurrent = (self.can_concurrent or self.type == 'web') and host_concurrent
 
         accounts = host.accounts.all().filter(is_active=True, privileged=False)
         private_account = accounts.filter(username='js_{}'.format(user.username)).first()
