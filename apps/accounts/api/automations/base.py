8421d8c91cd3348da7cddb734bb65c88118e5555
skysecs <100838405+skysecs@users.noreply.github.com>
2023-08-02 10:30:05 +0000

    perf: 修改 telnet 平台 setting

--- apps/accounts/api/automations/base.py
+++ apps/accounts/api/automations/base.py
@@ -95,8 +95,8 @@ class AutomationExecutionViewSet(
     mixins.CreateModelMixin, mixins.ListModelMixin,
     mixins.RetrieveModelMixin, viewsets.GenericViewSet
 ):
-    search_fields = ('trigger', 'automation__name')
-    filterset_fields = ('trigger', 'automation_id', 'automation__name')
+    search_fields = ('trigger',)
+    filterset_fields = ('trigger', 'automation_id')
     serializer_class = serializers.AutomationExecutionSerializer
 
     tp: str
