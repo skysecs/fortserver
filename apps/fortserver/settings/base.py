4311dd8060bbb66b0fd1aadfed655dc4291b522a
skysecs <100838405+skysecs@users.noreply.github.com>
2023-11-16 10:15:04 +0000

    perf: windows 改密推送添加新的方式 最后测试可连接性的时候采用rdp的方式测试

--- apps/fortserver/settings/base.py
+++ apps/fortserver/settings/base.py
@@ -160,7 +160,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'fortserver.middleware.TranslatePatchMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
