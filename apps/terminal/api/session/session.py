c351aa75b2893759d67a6c2a5cef274a8c256404
skysecs <100838405+skysecs@users.noreply.github.com>
2023-11-28 06:50:04 +0000

    perf: 随机密码生成规则添加可排除字符选项

--- apps/terminal/api/session/session.py
+++ apps/terminal/api/session/session.py
@@ -62,7 +62,7 @@ class SessionFilterSet(BaseFilterSet):
     class Meta:
         model = Session
         fields = [
-            "user", "user_id", "asset", "account", "remote_addr",
+            "user", "asset", "account", "remote_addr",
             "protocol", "is_finished", 'login_from', 'terminal'
         ]
 
