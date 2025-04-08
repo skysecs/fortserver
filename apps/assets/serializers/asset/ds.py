2e06cd1a341165b1a2692dc528e404a333d0cc11
skysecs <100838405+skysecs@users.noreply.github.com>
2025-04-08 11:25:10 +0000

    perf: rename ad to ds

---
+++ apps/assets/serializers/asset/ds.py
@@ -0,0 +1,22 @@
+from django.utils.translation import gettext_lazy as _
+
+from assets.models import DirectoryService
+from .common import AssetSerializer
+
+__all__ = ['DSSerializer']
+
+
+class DSSerializer(AssetSerializer):
+    class Meta(AssetSerializer.Meta):
+        model = DirectoryService
+        fields = AssetSerializer.Meta.fields + [
+            'domain_name',
+        ]
+        extra_kwargs = {
+            **AssetSerializer.Meta.extra_kwargs,
+            'domain_name': {
+                'help_text': _(
+                    'The domain name of the Active Directory'
+                ),
+                'label': _('Domain name')}
+        }
