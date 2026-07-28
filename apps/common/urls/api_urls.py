0561a2c46d3cf867cb1967533d3a7535c67b2cd1
skysecs <100838405+skysecs@users.noreply.github.com>
2026-07-28 08:40:07 +0000

    fix: lookup filter for unallowed fields

--- apps/common/urls/api_urls.py
+++ apps/common/urls/api_urls.py
@@ -12,8 +12,3 @@ urlpatterns = [
     path('resources/cache/', api.ResourcesIDCacheApi.as_view(), name='resources-cache'),
     path('countries/', api.CountryListApi.as_view(), name='resources-cache'),
 ]
-
-if settings.JDMC_ENABLED:
-    urlpatterns.append(
-        path('jdmc/sso-token/', api.JdmcSSOTokenAPI.as_view(), name='jdmc-sso-token')
-    )
